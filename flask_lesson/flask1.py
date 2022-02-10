b84332e5be4eb27564295bb31c10a5159cb8827e
Alex-ast7 <alex7ast@gmail.com>
2022-02-10 16:26:46 +0400

    решение задачи Варианты выбора

--- flask_lesson/flask1.py
+++ flask_lesson/flask1.py
@@ -199,5 +199,34 @@ def selection():
 
         return "Форма отправлена"
 
+@app.route('/choice/<planet_name>')
+def choice(planet_name):
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                  </head>
+                  <body>
+                    <h1>Моё предложение: {planet_name}</h1>
+                    <div>
+                      Эта планета близка к Земле
+                    </div>
+                    <div class="alert alert-primary" role="alert">
+                      На ней много необходимо ресурсов
+                    </div>
+                    <div class="alert alert-secondary" role="alert">
+                      На ней есть вода и атмосфера
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                      На ней есть набольшое магнитное поле
+                    </div>
+                    <div class="alert alert-success" role="alert">
+                      Наконец, она просто красива!
+                    </div>
+                  </body>
+                </html>"""
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
