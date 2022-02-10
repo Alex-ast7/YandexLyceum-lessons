584dc2fe2085c8e51e547868f9752c3176fa7574
Alex-ast7 <alex7ast@gmail.com>
2022-02-10 17:50:36 +0400

    решение задачи Тренировки в полёте

--- web/web_html1.py
+++ web/web_html1.py
@@ -9,5 +9,11 @@ app = Flask(__name__)
 def index(new_title):
     return render_template('index.html', title=new_title)
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('training.html', prof=prof)
+
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
