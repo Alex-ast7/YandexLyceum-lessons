db9a2b9d7639ee5a8f9f13cd8518b3153a061173
Alex-ast7 <alex7ast@gmail.com>
2022-02-10 16:46:08 +0400

    решение задачи Результат отбора

--- flask_lesson/flask1.py
+++ flask_lesson/flask1.py
@@ -228,5 +228,31 @@ def choice(planet_name):
                     </div>
                   </body>
                 </html>"""
+@app.route('/results/<nickname>/<int:level>/<float:rating>')
+def results(nickname, level, rating):
+    return f"""<!doctype html>
+                <html lang="en">
+                  <head>
+                    <meta charset="utf-8">
+                    <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                    <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
+                  </head>
+                  <body>
+                    <h1>Результаты отбора</h1>
+                    <div>
+                      Претендента на участие в миссии {nickname}
+                    </div>
+                    <div class="alert alert-primary" role="alert">
+                      Поздравляем! Ваш рейтинг после {level} этапа отбора
+                    </div>
+                    <div>
+                      составляет {rating}
+                    </div>
+                    <div class="alert alert-danger" role="alert">
+                      Желаем удачи!
+                    </div>
+                  </body>
+                </html>"""
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
