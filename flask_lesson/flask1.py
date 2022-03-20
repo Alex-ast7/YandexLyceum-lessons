1b4415eceb882f6adb3902645aa0bcfb78b48025
Alex-ast7 <alex7ast@gmail.com>
2022-03-20 12:22:59 +0400

    решение задачи Пейзажи Марса

--- flask_lesson/flask1.py
+++ flask_lesson/flask1.py
@@ -45,6 +45,7 @@ def image_mars():
                   </body>
                 </html>'''
 
+
 @app.route('/promotion_image')
 def promotion_image():
     return f"""<!doctype html>
@@ -75,6 +76,7 @@ def promotion_image():
                     </div>
                   </body>
                 </html>"""
+
 
 @app.route('/selection', methods=['POST', 'GET'])
 def selection():
@@ -201,6 +203,7 @@ def selection():
 
         return "Форма отправлена"
 
+
 @app.route('/choice/<planet_name>')
 def choice(planet_name):
     return f"""<!doctype html>
@@ -230,6 +233,8 @@ def choice(planet_name):
                     </div>
                   </body>
                 </html>"""
+
+
 @app.route('/results/<nickname>/<int:level>/<float:rating>')
 def results(nickname, level, rating):
     return f"""<!doctype html>
@@ -256,6 +261,7 @@ def results(nickname, level, rating):
                     </div>
                   </body>
                 </html>"""
+
 
 @app.route('/load_photo', methods=['POST', 'GET'])
 def load_photo():
@@ -290,5 +296,51 @@ def load_photo():
         f = request.files['file']
         print(f.save('static/img/img.jpg'))
         return "Форма отправлена"
+
+
+@app.route('/carousel')
+def carousel():
+    return f'''<!doctype html>
+                        <html lang="en">
+                          <head>
+                            <meta charset="utf-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+                            <title>Пример формы</title>
+                          </head>
+                          <body>
+                            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="/static/img/mars.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="/static/img/mars2.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="/static/img/mars3.jpg" alt="Third slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+                          </body>
+                        </html>'''
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
